f26f0dcab6efc44260d1265861f6e4541b9f8e26
Astor Levy <102769883+AstorLevy@users.noreply.github.com>
2022-05-28 16:06:30 -0300

    add: Mercado liebre adaptative version

---
+++ app.js
@@ -0,0 +1,25 @@
+//Requiero y creo las constantes express y path---------------------------------------------------------
+const express = require('express');
+const path = require('path');
+
+//creo la variable app que ejecuta express--------------------------------------------------------------
+let app = express(); 
+
+app.use(express.urlencoded({extended:false}));
+
+//Declaro la ruta de la cual obtendre los recursos estaticos--------------------------------------------
+const publicPath = path.join(__dirname,'/public');
+app.use(express.static(publicPath));
+
+const port = process.env.PORT || 3000
+
+//Defino la entrada del puerto--------------------------------------------------------------------------
+app.listen(port, ()=>{
+    console.log(`Escuchando por el puerto ${port}`)
+});
+
+//Defino las rutas de HTML------------------------------------------------------------------------------
+const homePath = path.join(__dirname, "/views/home.html")
+app.get("/",(req, res)=>{
+res.sendFile(homePath);
+})
